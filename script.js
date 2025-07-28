723481455eb186f3ea3a1511dae98f44faa51131
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-07-28 11:40:59 +0600

    Score Page

--- script.js
+++ script.js
@@ -40,8 +40,23 @@ let finalTimeDisplay = '0.0s';
 // Scroll
 let valueY =0;
 
+// Reset Game
+function playAgain() {
+  gamePage.addEventListener('click', startTimer);
+  scorePage.hidden = true;
+  splashPage.hidden = false;
+  equationsArray = [];
+  playerGuessArray = [];
+  valueY = 0;
+  playAgainBtn.hidden = true;
+}
+
 // Show Score Page
 function showScorePage() {
+  // Show Play Again button after 1 second
+  setTimeout(() => {
+    playAgainBtn.hidden = false;
+  }, 1000);
   gamePage.hidden = true;
   scorePage.hidden = false;
 }
@@ -51,9 +66,11 @@ function scoresToDOM() {
   finalTimeDisplay = finalTime.toFixed(1);
   baseTime = timePlayed.toFixed(1);
   penaltyTime = penaltyTime.toFixed(1);
-  baseTimeEl.textContent = `Base Time: ${baseTime}`;
+  baseTimeEl.textContent = `Base Time: ${baseTime}s`;
   penaltyTimeEl.textContent = `Penalty Time: ${penaltyTime}s`;
   finalTimeEl.textContent = `${finalTimeDisplay}s`;
+  // Scroll to Top, go to Score page
+  itemContainer.scrollTo({ top:0, behavior: 'instant' });
   showScorePage();
 }
 
